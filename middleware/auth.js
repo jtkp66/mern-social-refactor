f4888e7dec4ba1110b62fe8ccde8606213f3e81d
Jamie Pham <jtkp66@gmail.com>
2019-05-07 14:02:00 -0700

    Added custom middleware to check for toke, verify, and add user to payload.

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+// when you have a middleware functionn you pass in 3 things res, req, next
+module.exports = function auth(req, res, next) {
+  const token = req.header("x-auth-token");
+
+  // Check for token
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorization denied" });
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    // Add user from payload
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).json({ msg: "Token is not valid" });
+  }
+};
