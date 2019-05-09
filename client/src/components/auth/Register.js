367b6948320f7309ef35e6f541527f8aa8784e9a
Jamie Pham <jtkp66@gmail.com>
2019-05-08 17:15:18 -0700

    Finished alert actions and reducers for error checking and validation

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = props => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -17,7 +20,7 @@ const Register = () => {
   const onSubmit = e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords do not match");
+      props.setAlert("Passwords do not match", "danger");
     } else {
       console.log(formData);
     }
@@ -82,4 +85,16 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+// const mapStateToProps = state => {
+
+// }
+
+export default connect(
+  null,
+  { setAlert }
+)(Register);
+// Allows props.setAlert
